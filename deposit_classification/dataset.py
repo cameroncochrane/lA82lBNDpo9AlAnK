debdd4a3e6281d43d4d7d8872d5806a0b40395ef
cameroncochrane <101337073+cameroncochrane@users.noreply.github.com>
2026-06-01 20:00:38 +0100

    Worked on imputation strategies to deal with 'unknown' values in three of the columns. Involves deletion and grouped mode strategy.

--- deposit_classification/dataset.py
+++ deposit_classification/dataset.py
@@ -14,7 +14,7 @@ app = typer.Typer()
 @app.command()
 def main(
     input_path: Path = RAW_DATA_DIR / "term-deposit-marketing-2020.csv",
-    output_path: Path = INTERIM_DATA_DIR / "interim_dataset.csv",
+    output_path: Path = INTERIM_DATA_DIR / "xxxx",
 ):
     raw_data = pd.read_csv(input_path)
     
